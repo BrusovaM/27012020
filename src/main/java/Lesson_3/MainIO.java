529fd42092c3cb3b386a4f7d87f1e84858fa2add
BrusovaM <ardipitek83@gmail.com>
2020-02-05 21:08:01 +0500

    Initial commit

---
+++ src/main/java/Lesson_3/MainIO.java
@@ -0,0 +1,11 @@
+package Lesson_3;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainIO {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+    }
+}
